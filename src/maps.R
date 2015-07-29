1af723a1a6480de5a3774d0cdf6daefd0d7f926f
alephcero <4lephcero@gmail.com>
2015-07-29 19:31:45 -0300

    creating/loading data for maps

--- src/maps.R
+++ src/maps.R
@@ -8,24 +8,89 @@
 #14_10_2 Esta es la zona de lagos de palermo la cartografia de ciudad tiene los dos radios de esa fraccion 14_10_2 y 14_10_1 (para esta ultima hay data)
 #1_13_15 no esta porque En censo no esta #"AREA#020011315" esta es la tabla vacia, es un radio en puerto madero
 
+#Mapa BA
  
- 
-#CONDHAB
-
-baseCondhab = leerTablasRedatam(archivo = "data/CENSO/condhab/condhabREDATAM.csv")
-write.csv(baseConhab,"bases/Censo/exportacionesRedatamOnline/condhab/baseCondhab.csv",row.names = F)
+caba = readOGR(dsn = "maps/radiosBA", layer = "radios_censo_2010")
 
 
-caba = readOGR(dsn = "bases/Censo/cartografia", layer = "radios_censo_2010")
+#Lectura y modificacion de bases
+
+##CONDHAB
+archivo.condhab = "data/CENSO/condhab/baseCondhab.csv" 
+if (file.exists(archivo.condhab)) {
+  baseCondhab = read.csv(archivo.condhab)
+} else {
+  baseCondhab = leerTablasRedatam(archivo = "data/CENSO/condhab/condhabREDATAM.csv")
+  write.csv(baseConhab,archivo.condhab,row.names = F)
+  baseCondhab = read.csv(archivo.condhab)
+}
+names(baseCondhab) = gsub(" ",".",names(baseCondhab))
+
+baseCondhab = 
+  baseCondhab %>%
+  mutate(NoSuficiente = Parcialmente.insuficiente + Insuficiente) %>%
+  select(radios,LINK,comuna,CO_FRAC_RA,NoSuficiente)
+
+
+##NBI
+
+archivo.NBI = "data/CENSO/nbi/baseNBI.csv" 
+if (file.exists(archivo.NBI)) {
+  baseNBI = read.csv(archivo.NBI)
+} else {
+  baseNBI = leerTablasRedatam(archivo = "data/CENSO/nbi/nbiREDATAM.csv", chequeo = TRUE)
+  write.csv(baseNBI,archivo.NBI,row.names = F)
+  baseNBI = read.csv(archivo.NBI)
+}
+
+##Hacinamiento
+
+archivo.hacinamiento = "data/CENSO/hacinamiento/baseHacinamiento.csv" 
+if (file.exists(archivo.hacinamiento)) {
+  baseHacinamiento = read.csv(archivo.hacinamiento)
+} else {
+  baseHacinamiento = leerTablasRedatam(archivo = "data/CENSO/hacinamiento/hacinamientoREDATAM.csv", chequeo = TRUE)
+  write.csv(baseHacinamiento,archivo.hacinamiento,row.names = F)
+  baseHacinamiento = read.csv(archivo.hacinamiento)
+}
+
+##Jefe Argentino
+
+archivo.jefeArgentino = "data/CENSO/jefeArgentino/baseJefeArgentino.csv" 
+if (file.exists(archivo.jefeArgentino)) {
+  baseJefeArgentino = read.csv(archivo.jefeArgentino)
+} else {
+  baseJefeArgentino = leerTablasRedatam(archivo = "data/CENSO/jefeArgentino/jefeArgentinoREDATAM.csv", chequeo = TRUE)
+  write.csv(baseJefeArgentino,archivo.jefeArgentino,row.names = F)
+  baseJefeArgentino = read.csv(archivo.jefeArgentino)
+}
+
+##Capeco
+
+archivo.capeco = "data/CENSO/capeco/baseCapeco.csv" 
+if (file.exists(archivo.capeco)) {
+  baseCapeco = read.csv(archivo.capeco)
+} else {
+  baseCapeco = leerTablasRedatam(archivo = "data/CENSO/capeco/capecoREDATAM.csv")
+  write.csv(baseCapeco,archivo.capeco,row.names = F)
+  baseCapeco = read.csv(archivo.capeco)
+}
+
+
+#Join de data al mapa
 
 caba@data = left_join(caba@data,baseCondhab)
+caba@data = left_join(caba@data,baseCapeco)
+#caba@data = left_join(caba@data,baseNBI)
+#caba@data = left_join(caba@data,baseExtrangero)
 
-caba@data$NoSuficiente = caba@data$`Parcialmente insuficiente` + caba@data$Insuficiente 
+
 
 comuna = !is.na(caba@data$comuna) & caba@data$comuna == 8
 
 plot(caba[completo,])
 
+#Mapeo
 
 qtm(caba, "NoSuficiente",fill.palette="Reds") # plot the basic map
 qtm(caba[comuna,],"NoSuficiente",fill.palette="Reds") # plot the basic map
@@ -35,81 +100,6 @@ qtm(caba[comuna,],"NoSuficiente",fill.palette="Reds") # plot the basic map
 tm_shape(caba) +
   tm_fill("NoSuficiente", thres.poly = 0) +
   tm_layout(legend.show = FALSE, title.position = c("right", "bottom"), title.size = 3)
-
-
-#CALMAT
-baseCalmat = leerTablasRedatam(archivo = "bases/Censo/exportacionesRedatamOnline/calmat/calmatNuevo.csv")
-write.csv(baseCalmat,"bases/Censo/exportacionesRedatamOnline/calmat/baseCalmat.csv",row.names = F)
-
-caba = readOGR(dsn = "bases/Censo/cartografia", layer = "radios_censo_2010")
-
-caba@data = left_join(caba@data,baseCalmat)
-
-caba@data$CalmatBajo = caba@data$`Calidad 3` + caba@data$`Calidad 4` 
-
-comuna = !is.na(caba@data$comuna) & caba@data$comuna == 1
-
-
-qtm(caba, "CalmatBajo",fill.palette="Reds") # plot the basic map
-qtm(caba[comuna,],"CalmatBajo",fill.palette="Reds") # plot the basic map
-
-#CAPECO
-baseCapeco = leerTablasRedatam(archivo = "bases/Censo/exportacionesRedatamOnline/CAPECO/capecoCABAnuevo.csv")
-write.csv(baseCapeco,"bases/Censo/exportacionesRedatamOnline/CAPECO/baseCapeco.csv",row.names = F)
-
-baseCapeco$quintil5= baseCapeco$`10` + baseCapeco$`9`
-
-caba@data = left_join(caba@data,baseCapeco)
-
-comuna = !is.na(caba@data$comuna) & caba@data$comuna == 1
-
-qtm(caba, "quintil5") # plot the basic map
-qtm(caba[comuna,], "jefeUnivQ") # plot the basic map
-
-
-
-
-#Tenencia
-baseTenencia = leerTablasRedatam(archivo = "bases/Censo/exportacionesRedatamOnline/regimenTenencia/regimenTenenciaNuevo.csv",chequeo = TRUE)
-write.csv(baseTenencia,"bases/Censo/exportacionesRedatamOnline/regimenTenencia/baseTenencia.csv",row.names = F)
-
-baseTenencia$prop = baseTenencia$`Propietario de la vivienda y del terreno` + baseTenencia$`Propietario sólo de la vivienda`
-
-caba@data = left_join(caba@data,baseTenencia)
-
-comuna = !is.na(caba@data$comuna) & caba@data$comuna == 1
-
-qtm(caba, "prop") # plot the basic map
-qtm(caba[comuna,], "prop") # plot the basic map
-
-
-caca1 = select(baseCapeco,LINK,quintil5)
-caca2 = select(baseTenencia,LINK,prop)
-
-caca = merge(caca1,caca2)
-
-ggplot(caca, aes(x=quintil5, y=prop)) +
-  geom_point(shape=1) +    # Use hollow circles
-  geom_smooth(method=lm)
-
-
-
-
-
-#Fuente CIUDAD
-
-caba = readOGR(dsn = "maps/radiosBA", layer = "radios_censo_2010")
-caba@data = left_join(caba@data,jefeUniversitario)
-
-
-comuna = !is.na(caba@data$comuna) & caba@data$comuna == 1
-
-plot(caba[completo,])
-
-
-qtm(caba, "jefeUniv") # plot the basic map
-qtm(caba[comuna,], "jefeUnivQ") # plot the basic map
-
 
 
 tm_shape(caba) +
