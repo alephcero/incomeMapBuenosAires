572537104eb03485c7c80773661a12d748b65768
pipegonz <felipegonz@gmail.com>
2015-08-04 17:12:01 -0300

    minor changes to function's and libraries's scripts

--- src/functions.R
+++ src/functions.R
@@ -1,3 +1,30 @@
 #Functions
-redatamFunction = url("https://raw.githubusercontent.com/alephcero/REDATAMtoR/master/retadamFrequenciesToR.R",method = "libcurl")
-source(redatamFunction)
+
+pasarAutf8 = function(texto.factor) {
+  etiquetas = levels(texto.factor)
+  etiquetas = iconv(etiquetas, "latin1", "utf-8")
+  texto.factor = iconv(texto.factor, "latin1", "utf-8")
+  texto.factor = factor(texto.factor,levels = etiquetas)
+  texto.factor
+}
+
+
+redatamFunction = "https://raw.githubusercontent.com/alephcero/REDATAMtoR/master/retadamFrequenciesToR.R"
+redatamFunctionAverages = "https://raw.githubusercontent.com/alephcero/REDATAMtoR/master/redatamAveragesToR.R"
+
+source_https <- function(url) {
+  # load package
+  require(RCurl)
+  
+  # parse and evaluate each .R script
+  script <- getURL(url, ssl.verifypeer = FALSE)
+  eval(parse(text = script))
+}
+
+# Example
+
+script <- getURL(redatamFunctionAverages, ssl.verifypeer = FALSE)
+eval(parse(text = script))
+
+script <- getURL(redatamFunction, ssl.verifypeer = FALSE)
+eval(parse(text = script))
